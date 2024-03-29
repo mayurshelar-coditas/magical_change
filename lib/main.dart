ab08e6d7ea4017697f983315931ca6baed8868c9
mayurshelar-coditas <mayur.shelar@coditas.com>
2024-03-29 12:04:15 +0530

    Assignment 6 Part 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+
 import 'package:magical_change/screens/home_screen.dart';
+
 
 void main() => runApp(
       ScreenUtilInit(
-        designSize: const Size(430, 932),
-        builder: (_, child) {
-          return MaterialApp(
-            debugShowCheckedModeBanner: false,
-            home: child,
-          );
-        },
-        child: const HomeScreen(),
-      ),
+          designSize: const Size(430, 932),
+          builder: (_, child) {
+            return MaterialApp(
+              debugShowCheckedModeBanner: false,
+              home: child,
+            );
+          },
+          child: const HomeScreen(),
+        ),
+      
     );
